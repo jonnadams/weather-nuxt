3889acae971a2f45fcea0cd12264572a36c56c50
Jonathan Adams <adams.jonathan@outlook.com>
2025-11-14 22:27:32 -0500

    Install Next/UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/image', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/ui'],
   css: ['~/assets/css/main.css'],
 })
